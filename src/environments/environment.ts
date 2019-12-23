c50ac050fa54d54e887b0f373039a22ad8ab32d5
Pruvotfabian <pruvotfabian@gmail.com>
2019-12-23 13:49:35 +0100

    projet historikaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: true,
-
+  firebaseConfig:{
+    apiKey: "AIzaSyDOIrx4OnmJKeW__JIQk8GxK7t3IG9M8JQ",
+    authDomain: "historikaa.firebaseapp.com",
+    databaseURL: "https://historikaa.firebaseio.com",
+    projectId: "historikaa",
+    storageBucket: "historikaa.appspot.com",
+    messagingSenderId: "600307971035",
+    appId: "1:600307971035:web:62453acc5c4f5e56"
+  }
 };
 
 /*
